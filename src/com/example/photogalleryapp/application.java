08fd899b4edf995178d26dbf513687c875762a1c
roodlandy <78751319+roodlandy@users.noreply.github.com>
2023-01-24 11:23:34 -0500

    Created application main class and added mvc project structure

---
+++ src/com/example/photogalleryapp/application.java
@@ -0,0 +1,19 @@
+package com.example.photogalleryapp;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class application extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,10 +1,10 @@
 module com.example.photogalleryapp {
     requires javafx.controls;
     requires javafx.fxml;
-            
-        requires org.controlsfx.controls;
-                        requires org.kordamp.bootstrapfx.core;
-            
+
+    requires org.controlsfx.controls;
+    requires org.kordamp.bootstrapfx.core;
+
     opens com.example.photogalleryapp to javafx.fxml;
     exports com.example.photogalleryapp;
 }
